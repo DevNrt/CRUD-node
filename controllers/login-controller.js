cd17d454580bc866deeb4946e2021fa02c5505d8
Nicolas <nik891011@gmail.com>
2021-01-13 00:23:52 -0500

    validaciones

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,40 +1,55 @@
 const db = require('../config/db')
+const bcrypt = require('bcrypt')
 
 function login(req,res){
-    //capturo variable
-    //encripto
-    //comparo
-    const loginQuery = "SELECT * FROM users WHERE username='"+req.body.username+"' AND password='"+req.body.password+"'"
 
-    db.query(loginQuery,(err,result) =>{
-        if(err){
-            res.send(err)
-        } else{
-            console.log(result.length)
-            if(result.length == 0){
-                res.send('usuario no encontrado')
+    const loginQuery = "SELECT * FROM users WHERE username='"+req.body.username+"'"
+     
+        db.query(loginQuery,(err,result) =>{
+            if(err){
+                res.status(500).send(err)
             } else{
-                res.send('usuario  encontrado')
+                if(result.length == 0){
+                    res.status(400).send('usuario no encontrado')
+                } else{
+                    bcrypt.compare(req.body.pass,result[0].pass,function(err,data){
+                        if(data == true){
+                            res.status(200).send({
+                                result: result[0].pass,
+                                status: '200',
+                                text:'Usuario encontrado'
+                            })
+                        } else{
+                            res.status(400).send('Datos incorrectos');
+                        }
+                    });
+              }
             }
-        }
-    })
+        })
 
 }
 
 function insertLogin(req,res){
-    //capturo variable
-    //encriptar va antes del query
-    const sqlInsert = "INSERT INTO users (username,password) VALUES ('"+req.body.username+"','"+req.body.password+"')"
+    const pass = req.body.pass
+    bcrypt.hash(pass,10,function(err,data){
+        if(data){
+            const sqlInsert = "INSERT INTO users (username,pass) VALUES ('"+req.body.username+"','"+data+"')"
 
-    db.query(sqlInsert, (err, result) => {
+            db.query(sqlInsert, (err, result) => {
 
-    if(err){
-        res.send(err)
-    } else{
-        res.send("True")
-    }
-
-    })
+                if(err){
+                    res.send(err)
+                } else{
+                    res.send("True")
+                }
+            
+                })
+        } else{
+            res.send(err)
+        }
+    });
+    
+    
 }
 
 module.exports={
--- routes/login-routes.js
+++ routes/login-routes.js
@@ -3,5 +3,6 @@ const api = express.Router()
 const logincontrollers = require('../controllers/login-controller')
 
 api.post("/login",logincontrollers.login)
+api.post("/insertLogin",logincontrollers.insertLogin)
 
 module.exports = api
