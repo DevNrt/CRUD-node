8c3522bc27eb6cb3ad5e8a4cec77e76200ea5260
Nicolas <nik891011@gmail.com>
2021-01-16 20:54:07 -0500

    jwt

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,10 +1,11 @@
 const db = require('../config/db')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 function login(req,res){
 
     const loginQuery = "SELECT * FROM users WHERE username='"+req.body.username+"'"
-     
+        
         db.query(loginQuery,(err,result) =>{
             if(err){
                 res.status(500).send(err)
@@ -14,11 +15,27 @@ function login(req,res){
                 } else{
                     bcrypt.compare(req.body.pass,result[0].pass,function(err,data){
                         if(data == true){
-                            res.status(200).send({
-                                result: result[0].pass,
-                                status: '200',
-                                text:'Usuario encontrado'
-                            })
+                            const user ={
+                                username: req.body.username,
+                                pass: req.body.pass
+                            }
+                            jwt.sign({user:user}, 'Q1az &yHN 9oL', (err,token) =>{
+                                if(err){
+                                    res.status(403);
+                                }else{
+                                    jwt.verify(token,'Q1az &yHN 9oL',(err,authData) =>{
+                                        if(err){
+                                            res.status(403);
+                                        }else{
+                                            res.status(200).send({
+                                                token: token,
+                                                status: '200',
+                                                text:'Usuario encontrado'
+                                            })
+                                        }
+                                    }) 
+                                }   
+                            })  
                         } else{
                             res.status(400).send('Datos incorrectos');
                         }
@@ -51,6 +68,9 @@ function insertLogin(req,res){
     
     
 }
+
+
+   
 
 module.exports={
     login,
